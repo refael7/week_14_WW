6cbe0176d40e4ee66abe69fe302e6a2dff992cf6
refael_saidov <refaelsaidov1@gmail.com>
2026-01-22 13:00:52 +0200

    d

--- weapon-warehouse-system/app/db.py
+++ weapon-warehouse-system/app/db.py
@@ -1,24 +1,22 @@
 import mysql.connector
 from mysql.connector.connection import MySQLConnectionAbstract
+import os
 
 
-
-def get_connection():
-
+def connection():
     conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="weather_db"
-    )
+    host=os.getenv("MYSQL_ROOT_HOST") ,
+    user=os.getenv("MYSQL_ROOT_USER"),
+    password=os.getenv("MYSQL_ROOT_PASSWORD"),
+    database=os.getenv("MYSQL_ROOT_DB"))
     return conn
 
 
 
 
-def create_table(mydb:MySQLConnectionAbstract):
-    my_cursor = mydb.cursor()
-    my_cursor.execute("""
+def create_table():
+    conn = connection()
+    return conn.cursor().execute("""
     CREATE TABLE IF NOT EXIST weapons_list (
                     id INT PRIMARY KEY AUTO_INCREMENT,
                     weapon_id VARCHAR(250), 
@@ -32,34 +30,21 @@ def create_table(mydb:MySQLConnectionAbstract):
                     year_estimated INT, 
                     level_risk VARCHAR(250))""")
 
-def insert_weapons_list(conn, data: dict):
-    query = """
-    INSERT INTO weather_records (
-        weapon_id , 
-        weapon_name ,
-        weapon_type , 
-        range_km ,
-        weight_kg ,
-        manufacturer , 
-        origin_country , 
-        storage_location  ,
-        year_estimated , 
-        level_risk 
-    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """
 
-    values = (
-        data["weapon_id"],
-        data["weapon_name"],
-        data["weapon_type"],
-        data["range_km"],
-        data["weight_kg"],
-        data["manufacturer"],
-        data["origin_country"],
-        data["storage_location"],
-        data["year_estimated"],
-        data["level_risk"]
-
-    )
+def insert_weapons_list():
+    conn = connection()
     cursor = conn.cursor()
-    cursor.execute(query, values)
+    insert_table = """
+        INSERT INTO weapons_list (
+        weapon_id,
+        weapon_name,
+        weapon_type,
+        range_km,
+        weight_kg,
+        manufacturer,
+        origin_country,
+        storage_location,
+        year_estimated,
+        risk_level ) 
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
--- weapon-warehouse-system/app/main.py
+++ weapon-warehouse-system/app/main.py
@@ -1,11 +1,21 @@
-import io
 
 from fastapi import FastAPI, UploadFile, File, HTTPException
 import uvicorn
 from models import add_columns_risk_level, add_unknown
 import pandas as pd
+from db import create_table ,insert_weapons_list
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan():
+    create_table()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+
 
 @app.post("/upload")
 def create_file(file: UploadFile = File(...)):
@@ -14,10 +24,13 @@ def create_file(file: UploadFile = File(...)):
     df = pd.read_csv(file.file)
     df = add_columns_risk_level (df)
     df = add_unknown(df)
+
     print(df)
-    return {"weapon": file.file}
+    return {"status": "success",
+            "inserted_records":20
+            }
 
 
 
 if __name__ == "__main__":
-     uvicorn.run("main:app",port=8004,host="localhost" ,reload=True)
+     uvicorn.run("main:app",port=8000,host="localhost" ,reload=True)
